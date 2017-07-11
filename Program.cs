dc4312e1baddd2b79878484cabad00205bc96171
Ed Sadler <ed_lorie@comcast.net>
2017-07-11 10:35:24 -0500

    complete

--- Program.cs
+++ Program.cs
@@ -7,20 +7,26 @@ namespace tuples
     {
         static void Main(string[] args)
         {
-    List<(string product, double amount, int quantity)> transactions = new List<(string, double, int)>();
+    List<(string product, float amount, int quantity)> transactions = new List<(string, float, int)>();
 
-    transactions.Add(("hammer", 2.00, 1));
-    transactions.Add(("nails", 3.50, 2));
-    transactions.Add(("screwdriver", 1.75, 1));
-    transactions.Add(("shovel", 5.50, 1));
-    transactions.Add(("rake", 4.25, 1));
+    transactions.Add(("hammer", 2.00f, 1));
+    transactions.Add(("nails", 3.50f, 2));
+    transactions.Add(("screwdriver", 1.75f, 1));
+    transactions.Add(("shovel", 5.50f, 1));
+    transactions.Add(("rake", 4.25f, 1));
 
     int totalItems = 0;
 
-    double totalSales = 0.0;
+    float totalSales = 0.00f;
 
-    foreach ((string product, double amount, int quanity) t in transactions)
-            Console.WriteLine("Hello World!");
+    foreach ((string product, float amount, int quantity) t in transactions){
+
+    totalItems += t.quantity;
+    totalSales += t.amount * (float) t.quantity;
+    }
+
+            Console.WriteLine($"Items Sold Today: {totalItems}");
+            Console.WriteLine($"Total Revenue: {totalSales:C}");
         }
     }
 }
